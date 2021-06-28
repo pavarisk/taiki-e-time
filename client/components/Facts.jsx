fd484595acb4e38cb687303d7a70a8666bf98dad
pavarisk <p.kakanat@gmail.com>
2021-06-29 08:45:00 +1200

    premerge

--- client/components/Facts.jsx
+++ client/components/Facts.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { Card, Image, Segment, Button, Icon } from 'semantic-ui-react'
 import { getFacts, getFactById } from '../apiClient'
 
+const randomInt = Math.floor((Math.random() + 1) * 3)
+
 function Facts (props) {
   const [facts, setFacts] = useState([])
+  const [fact, setFact] = useState([])
+  const [ranId, setRanId] = useState(randomInt)
   const [id, setId] = useState(1)
-  console.log(facts)
 
   useEffect(() => {
     factIs()
@@ -15,16 +18,17 @@ function Facts (props) {
     return getFactById(id)
       .then(fact => {
         console.log(fact)
-        return setFacts(fact)
+        return setFact(fact)
       })
       .catch(e => console.log(e))
   }
 
-  function changeId (id) {
-    if (id === 3) {
-      id = 1
-      return setId(id)
-    } else return setId(id + 1)
+  function changeId () {
+    // if (id === 3) {
+    //   id = 1
+    //   return setId(id)
+    // } else return setId(id + 1)
+    return setId(ranId)
   }
 
   return (
@@ -41,19 +45,19 @@ function Facts (props) {
     // )
     <>
       <Card centered>
-        <Image src={facts.image} wrapped ui={false} />
+        <Image src={fact.image} wrapped ui={false} />
         <Card.Content>
-          <Card.Header>{facts.factType}</Card.Header>
+          <Card.Header>{fact.factType}</Card.Header>
           <Card.Meta>
-            <span className='reference'>{facts.reference}</span>
+            <span className='reference'>{fact.reference}</span>
           </Card.Meta>
           <Card.Description>
-            {facts.factDetails}
+            {fact.factDetails}
           </Card.Description>
         </Card.Content>
       </Card>
       <Segment color='yellow' inverted>
-        <Button onClick={() => changeId(id)} animated>
+        <Button onClick={() => changeId()} animated>
           <Button.Content visible>Another One</Button.Content>
           <Button.Content hidden>
             <Icon name='refresh' />
